e7c3ca21fe595220b42d984892aeac921dee461a
Eswaran Krishnamoorthy <eswaran.krishnamoorthy@ericsson.com>
2017-03-30 16:37:49 -0500

    rabbitmq connection

--- test/esb.spec.js
+++ test/esb.spec.js
@@ -8,17 +8,23 @@ chai.expect();
 const expect = chai.expect;
 
 let esb;
-
+ 
 describe('Rabbitmq Connection', () => {
   before(() => {
     esb = new ESB();
   });
   describe('Validation and Connection Url', () => {
     it('should return the Connection Url', () => {
-      expect(esb.buildUrl()).to.be.equal('amqp://guest:guest@localhost:1441');
+      expect(esb.buildUrl()).to.be.equal('amqp://guest:guest@us00206474.ericsson.se:4369');
     });
   });
   describe('Checking Rabbitmq connection', () => {
-
+      it('should return the connection', () => {
+        return esb.connect().then(function(data){
+          console.log(data);
+          done();
+        })
+    //    esb.connect().should.eventually.equal("test").notify(done);
+      })
   });
 });
