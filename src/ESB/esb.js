99d31a2f1b90a2999c3060a210672bc2a975e0c1
Krishnamoorthy Eswaran <krishnamoorthy.eswaran@ericsson.com>
2017-04-01 11:45:18 -0500

    bus

--- src/ESB/esb.js
+++ src/ESB/esb.js
@@ -4,18 +4,25 @@ import globalMsg from './../../config/error-msg-en.json';
 
 export default class ESB {
      constructor() {
+       this._connection = null;
      }
-
+    get Connection() {
+      return this._connection;
+    }
+    get Channel() {
+      return this._channel;
+    }     
     connect() {
       let RabbitMQUrl = this.buildUrl();
 
       if (RabbitMQUrl !== '') {
         return new Promise((resolve, reject) => {
-          amqp.connect(RabbitMQUrl, function (error, connection) {
+          amqp.connect(RabbitMQUrl, (error, connection) => {
             if (error) {
               reject(error);
             }
             else {
+              this._connection = connection;
               resolve(connection);
             }
           });
@@ -40,16 +47,101 @@ export default class ESB {
         hasErrors = true;
         ErrorMsg = globalMsg.RabbitMQ.Error.HostEmpty;
       }
-      if (config.RabbitMQ.port === '') {
-        hasErrors = true;
-        ErrorMsg = globalMsg.RabbitMQ.Error.PortEmpty;
-      }
       if (!hasErrors) {
-        ESBUrl = 'amqp://' + config.RabbitMQ.username + ':' + config.RabbitMQ.password + '@' + config.RabbitMQ.host + ':' + config.RabbitMQ.port;
+        ESBUrl = 'amqp://' + config.RabbitMQ.username + ':' + config.RabbitMQ.password + '@' + config.RabbitMQ.host;
       }
       else {
         throw ErrorMsg;
       }
+      if (config.RabbitMQ.port && config.RabbitMQ.port !== 0) {
+        ESBUrl += ':' + config.RabbitMQ.port;
+      }
       return ESBUrl;
+    }
+
+    createChannel(connection) {
+      return new Promise((resolve, reject) => {
+        connection.createChannel((error, channel) => {
+          if (error) {
+            reject(error);
+          }
+          else {
+            this._channel = channel;
+            resolve(channel);
+          }
+        });
+      });
+    }
+
+    createQueue(channel) {
+      return new Promise((resolve, reject) => {
+        try {
+          let exchanges = config.RabbitMQ.Exchanges;
+          let myQueues = config.RabbitMQ.myQueues;
+
+          for (let exchange in exchanges) {
+            channel.assertExchange(exchange.name, exchange.type, { durable: exchange.durable });
+            for (let queue in exchange.queues) {
+              let q = myQueues.find((x) => x.Queue === queue.name);
+             
+              if (q) {
+                channel.assertQueue(queue.name, { durable: queue.durable, noAck: queue.Acknowledge}, (err, que) => {
+                  channel.bindQueue(que, exchange, queue.Keyword);
+                });;
+              }
+            }
+          }
+          resolve(exchanges);
+        }
+        catch (error) {
+          reject(error);
+        }
+      });
+    }
+
+    start() {
+      return new Promise((resolve, reject) => {
+        try {
+          this.connect().then((conn) => {
+            this._connection = conn;
+          }).createChannel(this._connection).then((ch) => {
+            this._channel = ch;
+          }).createQueue(this._channel).then((queue) => {
+            resolve(globalMsg.RabbitMQ.Message.QueueCreated);
+          });
+        }
+        catch (error) {
+          reject(error);
+        }
+      });
+    }
+
+    send(key, message) {
+      if (!this._connection) {
+        throw globalMsg.RabbitMQ.Error.ConnectionNotAvail;
+      }
+      if (!this._channel) {
+        throw globalMsg.RabbitMQ.Error.ConnectionNotLive;
+      }
+      this._channel.publish('', key, new Buffer(message), {persistent: true});
+    }
+
+    consume(Queue, callback) {
+      if (!this._connection) {
+        throw globalMsg.RabbitMQ.Error.ConnectionNotAvail;
+      }
+      if (!this._channel) {
+        throw globalMsg.RabbitMQ.Error.ConnectionNotLive;
+      }
+      this._channel.consume(Queue, callback);
+    }
+
+    closeChannel() {
+      try {
+        this._channel.close();
+      }
+      catch (error) {
+        throw error;
+      }
     }
 }
--- test/esb.spec.js
+++ test/esb.spec.js
@@ -15,16 +15,33 @@ describe('Rabbitmq Connection', () => {
   });
   describe('Validation and Connection Url', () => {
     it('should return the Connection Url', () => {
-      expect(esb.buildUrl()).to.be.equal('amqp://guest:guest@us00206474.ericsson.se:4369');
+      expect(esb.buildUrl()).to.be.equal('amqp://guest:guest@localhost');
     });
   });
-  describe('Checking Rabbitmq connection', () => {
-      it('should return the connection', () => {
-        return esb.connect().then(function(data){
-          console.log(data);
+  describe('Rabbitmq connection', () => {
+    it('should return the connection', (done) => {
+      return esb.connect().then(function (data) {
+        expect(esb.Connection).to.not.be.undefined;
+        done();
+      });
+    });
+    it('should return the channel', (done) => {
+      return esb.connect().then(function (data) {
+        esb.createChannel(data).then(function (data) {
+          expect(esb.Channel).to.not.be.undefined;
           done();
-        })
-    //    esb.connect().should.eventually.equal("test").notify(done);
-      })
+        });
+         //    esb.connect().should.eventually.equal("test").notify(done);
+      });
+    });
+    it('should return the queue', (done) => {
+      return esb.connect().then(function (connection) {
+        esb.createChannel(connection).then(function (channel) {
+          esb.createQueue(channel);
+          done();
+        });
+         //    esb.connect().should.eventually.equal("test").notify(done);
+      });
+    });
   });
 });
